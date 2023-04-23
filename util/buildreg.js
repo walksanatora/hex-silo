52c9bc67bfc4c87f3ed398364e01485e7094783f
walksanatora <walkerffo22@gmail.com>
2023-04-23 00:47:09 -0700

    fix buildreg

--- util/buildreg.js
+++ util/buildreg.js
@@ -1,6 +1,10 @@
 //simple nodejs script to download the hex pattern registry and pop it in data/
 const fs = require('fs');
 
+if (!fs.existsSync("./data")){
+  fs.mkdirSync("./data");
+}
+
 fetch("https://raw.githubusercontent.com/object-Object/vscode-hex-casting/main/src/data/registry.json")
     .then(data => data.json())
     .then(reg => {
