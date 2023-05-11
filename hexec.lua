cb1a2e438972dc0c76cfe37c724828929938d3dc
walksanatora <walkerffo22@gmail.com>
2023-05-10 18:12:00 -0700

    small changes

--- hexec.lua
+++ hexec.lua
@@ -5,32 +5,31 @@ local write = "back"
 
 local completion = require "cc.shell.completion"
 
-shell.setCompletionFunction(shell.getRunningProgram(),completion.build(
+shell.setCompletionFunction(shell.getRunningProgram(), completion.build(
     completion.file
 ))
 
 local function executeHex(hex)
     fp.writeIota(nil)
-    redstone.setAnalogOutput(write,15)
-    sleep(1)
-    redstone.setAnalogOutput(write,0)
+    redstone.setAnalogOutput(write, 15)
     local code
     if type(hex) == "string" then
         if not fs.exists(hex) then error("File path is invalid, does not exist") end
         if fs.isDir(hex) then error("File path is meant to point to a json file") end
-        local f = fs.open(hex,"r")
+        local f = fs.open(hex, "r")
         local d = f.readAll()
         f.close()
         code = textutils.unserializeJSON(d)
-        if type(code) ~="table" then error("Invalid json data") end
+        if type(code) ~= "table" then error("Invalid json data") end
     elseif type(hex) == "table" then
         code = hex
     else
-        error("Expected file path or table, got"..type(hex))
+        error("Expected file path or table, got" .. type(hex))
     end
     fp.writeIota(code)
     local c = 0
-    while redstone.getAnalogInput(read) ~= 0 do sleep() end
+    while redstone.getInput(read) do sleep(1) end
+    redstone.setAnalogOutput(write, 0)
     return fp.readIota()
 end
 
@@ -38,7 +37,7 @@ local file = ...
 if file == nil then error("Argument expected: file path") end
 
 term.clear()
-term.setCursorPos(1,1)
+term.setCursorPos(1, 1)
 textutils.pagedPrint(
     textutils.serialise(
         executeHex(...)
